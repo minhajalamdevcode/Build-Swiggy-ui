13aa5c764eac19ad7507eedd675cf8c10e7c218d
minhaj alam <myminhajalam392@gmail.com>
2025-08-11 22:44:51 +0530

    add code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
   return (
     <div className='w-[273px] shrink-0'>
-        <div className='h-[182px] rounded-[15px] overflow-hidden relative' >
-            <img className="object-cover w-full h-full" src="images/pjsaxa4bkfphmjeiatt6.jpeg" alt="" />
-            <div className='image-overlay-card absolute w-full h-full top-0 flex items-end p-2 text-[30px] text-white font-bold tracking-tighter' >
-                Items at 179
+        <div className=' group h-[182px] rounded-[15px] overflow-hidden relative' >
+            <img className=" group-hover:scale-110 duration-150 object-cover w-full h-full" src={props.image} alt="" />
+            <div className='image-overlay-card absolute w-full h-full top-0 flex items-end p-2 text-[20px] text-white font-bold tracking-tighter' >
+                {props.offer}
             </div>
         </div>
                <div className='p-3'>
-                   <h2 className='text-2xl font-light'>hello</h2>
-                   <div className='flex text-2xl font-light'>
-                    <div>*</div>
-                    <div>4.3.35-40 mins</div>
+                   <h2 className='text-2xl font-light'>{props.title}</h2>
+                   <div className='flex text-2xl font-light gap-2'>
+                    <div> <img  className="inline"src={props.icon} alt="" /> {props.rating}</div>
+                    <div>{props.minTime}-{props.maxTime} mins</div>
                    </div>
-                   <p className='text-[21px] break-words'>hhdiufkjdfhiudfhkjfidfhkjfiudfkdhfddfddfdfdd</p>
+                   <p className='text-[21px] break-words'>{props.name}
+                     <br />
+                     {props.place}
+                   </p>
                 </div>
     </div>
   )
--- src/components/Toprestaurant.jsx
+++ src/components/Toprestaurant.jsx
@@ -4,6 +4,8 @@ import Card from './Card';
 
 function Toprestaurant() {
    const [data,setdata]=useState([]);
+   const [slide, setslide] = useState(0);
+   
    const fetchTopRestaurant= async ()=>{
     const response = await fetch("/restaurantChains.json");
     const apidata = await response.json();
@@ -14,17 +16,26 @@ function Toprestaurant() {
        fetchTopRestaurant();
    },[])
 
+   const prevslide=()=>{
+      if(slide == 0) return false;
+        setslide(slide-1)
+     }
+     const nextslide=()=>{
+      if(Categories.length -3 == slide) return false;
+      setslide(slide+1)
+     }
+
   return (
     <div className="max-w-[1200px] mx-auto px-2">
           {/* Title + Buttons (Desktop view) */}
           <div className="flex items-center justify-between md:flex-row flex-col">
-            <div className="text-[25px] pt-[20px] font-bold text-center md:text-left">
+            <div className="text-[25px] pt-[20px] pb-[18px] font-bold text-center md:text-left">
               Top restaurant chains in Chhindwara
             </div>
     
             {/* Desktop buttons */}
             <div className="hidden md:flex">
-              <div
+              <div onClick={prevslide}
                 
                 className="cursor-pointer w-[30px] h-[30px] bg-[#e9e9ea] rounded-full mx-2 flex items-center justify-center duration-200 hover:bg-[#d8d9da]"
               >
@@ -34,26 +45,24 @@ function Toprestaurant() {
               <div
                 
                 className="cursor-pointer w-[30px] h-[30px] bg-[#e9e9ea] rounded-full mx-2 flex items-center justify-center duration-200 hover:bg-[#d8d9da]"
+                onClick={nextslide}
               >
                 <FaArrowRight />
               </div>
             </div>
           </div>
-          {/* <div className='w-[273px] border border-red-200 h-auto'>
-                <img src="images/Rolls.jpeg" alt="" className='w-full rounded-full'/>
-                <div className='w-[240px] '>
-                   <h2>hello</h2>
-                   <div className='flex'>
-                    <div>*</div>
-                    <div>4.3.35-40 mins</div>
-                   </div>
-                   <p>hhdiufkjdfhiudfhkjfidfhkjfiudfkdhf</p>
-                </div>
-          </div> */}
+          
           <div className='flex gap-5 overflow-hidden'>
-            <Card/>
+            {
+              data.map((items,index)=>{
+                   return <Card {...items} key={index}/>
+              })
+            }
+            
+            
           </div>
        
+      <hr  className='my-4 border-[1px] border-gray-200'/>
 
 
     </div>
