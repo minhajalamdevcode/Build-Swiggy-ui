0c50815228d74221694657e94a8f2160ac7cf62e
minhaj alam <myminhajalam392@gmail.com>
2025-08-11 17:10:26 +0530

    add restaurant components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Category from './components/Category'
+import Toprestaurant from './components/Toprestaurant'
 
 function App() {
   return (
     <>
      <Header/>
      <Category/>
+     <Toprestaurant/>
 
     </>
   )
